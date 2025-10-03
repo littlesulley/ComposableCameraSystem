960bab5ac0a2d5eda76f2f654e096e9d59046af9
littlesulley <qinghonghan97@gmail.com>
2025-10-03 12:00:18 +0800

    Look at Node

---
+++ Source/ComposableCameraSystem/Private/Nodes/ComposableCameraLookAtNode.cpp
@@ -0,0 +1,80 @@
+﻿// Copyright Sulley. All rights reserved.
+
+#include "Nodes/ComposableCameraLookAtNode.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+void UComposableCameraLookAtNode::OnBeginPlayNode_Implementation(const FComposableCameraPose& CurrentCameraPose)
+{
+	Interpolator_T = IsValid(SoftLookAtInterpolator) ? SoftLookAtInterpolator->BuildRotatorInterpolator() : nullptr;
+
+	if (LookAtType == EComposableCameraLookAtType::ByActor)
+	{
+		if (!LookAtActor)
+		{
+			return;
+		}
+		
+		if (USkeletalMeshComponent* Comp = LookAtActor->GetComponentByClass<USkeletalMeshComponent>())
+		{
+			SkeletalMeshComponentForLookAtActor = Comp;
+		}
+	}
+}
+
+void UComposableCameraLookAtNode::OnTickNode_Implementation(float DeltaTime,
+	const FComposableCameraPose& CurrentCameraPose, FComposableCameraPose& OutCameraPose)
+{
+	FVector CurrentLookAtPosition = FVector::ZeroVector;
+	
+	if (LookAtType == EComposableCameraLookAtType::ByPosition)
+	{
+		CurrentLookAtPosition = LookAtPosition;
+	}
+	else if (LookAtType == EComposableCameraLookAtType::ByActor)
+	{
+		if (SkeletalMeshComponentForLookAtActor && SkeletalMeshComponentForLookAtActor->DoesSocketExist(LookAtSocket))
+		{
+			CurrentLookAtPosition = SkeletalMeshComponentForLookAtActor->GetSocketLocation(LookAtSocket);
+		}
+		else if (LookAtActor)
+		{
+			CurrentLookAtPosition = LookAtActor->GetActorLocation();
+		}
+	}
+
+	FRotator ResultRotation = FRotator::ZeroRotator;
+	
+	if (LookAtConstraintType == EComposableCameraLookAtConstraintType::Hard)
+	{
+		ResultRotation = UKismetMathLibrary::FindLookAtRotation(OutCameraPose.Position, CurrentLookAtPosition);
+	}
+	else if (LookAtConstraintType == EComposableCameraLookAtConstraintType::Soft)
+	{
+		FRotator CurrentRotation = OutCameraPose.Rotation;
+		FRotator TargetRotation = UKismetMathLibrary::FindLookAtRotation(OutCameraPose.Position, CurrentLookAtPosition);
+		FRotator LookAtRotation = TargetRotation;
+		FRotator DeltaRotation = (TargetRotation - CurrentRotation).GetNormalized();
+		TargetRotation = (DeltaRotation * SoftLookAtWeight + CurrentRotation).GetNormalized();
+		
+		if (Interpolator_T)
+		{
+			Interpolator_T->Reset(CurrentRotation, TargetRotation);
+			TargetRotation = Interpolator_T->Run(DeltaTime);
+		}
+
+		FVector TargetVector = TargetRotation.RotateVector(FVector::ForwardVector);
+		FVector LookAtVector = LookAtRotation.RotateVector(FVector::ForwardVector);
+		float Degree = UKismetMathLibrary::DegAcos(TargetVector.Dot(LookAtVector));
+		
+		if (Degree > SoftLookAtRange)
+		{
+			float RangeRatio = (Degree - SoftLookAtRange) / Degree;
+			TargetRotation = ((LookAtRotation - TargetRotation).GetNormalized() * RangeRatio + TargetRotation).GetNormalized();
+		}
+
+		ResultRotation = TargetRotation;
+	}
+
+	OutCameraPose.Rotation = ResultRotation;
+}
---
+++ Source/ComposableCameraSystem/Public/Nodes/ComposableCameraLookAtNode.h
@@ -0,0 +1,80 @@
+﻿// Copyright Sulley. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ComposableCameraCameraNodeBase.h"
+#include "Interpolator/ComposableCameraInterpolatorBase.h"
+#include "ComposableCameraLookAtNode.generated.h"
+
+class UComposableCameraInterpolatorBase;
+
+UENUM()
+enum class EComposableCameraLookAtType : uint8
+{
+	// Target position.
+	ByPosition,
+
+	// Target actor.
+	ByActor
+};
+
+UENUM()
+enum class EComposableCameraLookAtConstraintType : uint8
+{
+	// Hard look at: player cannot control camera.
+	Hard,
+
+	// Soft look at: player can control camera around the look at direction.
+	Soft
+};
+
+/**
+ * Node for looking at some target position.
+ */
+UCLASS(NotBlueprintable, ClassGroup = ComposableCameraSystem)
+class COMPOSABLECAMERASYSTEM_API UComposableCameraLookAtNode : public UComposableCameraCameraNodeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnBeginPlayNode_Implementation(const FComposableCameraPose& CurrentCameraPose) override;
+	virtual void OnTickNode_Implementation(float DeltaTime, const FComposableCameraPose& CurrentCameraPose, FComposableCameraPose& OutCameraPose) override;
+
+public:
+	// Look at type, to look at a specified position or by an actor's position.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters)
+	EComposableCameraLookAtType LookAtType;
+
+	// Target look-at position.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters, meta = (EditCondition = "LookAtType == EComposableCameraLookAtType::ByPosition", EditConditionHides))
+	FVector LookAtPosition;
+
+	// Target look-at actor.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters, meta = (EditCondition = "LookAtType == EComposableCameraLookAtType::ByActor", EditConditionHides))
+	AActor* LookAtActor;
+
+	// Target look-at socket when the actor has a SkeletalMeshComponent. If no such component exists, will use LookAtActor's position.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters, meta = (EditCondition = "LookAtType == EComposableCameraLookAtType::ByActor", EditConditionHides))
+	FName LookAtSocket;
+
+	// Look-at constraint type, hard look at or soft look at.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters)
+	EComposableCameraLookAtConstraintType LookAtConstraintType;
+
+	// Soft look at range in degrees.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters, meta = (EditCondition = "LookAtConstraintType == EComposableCameraLookAtConstraintType::Soft", EditConditionHides))
+	float SoftLookAtRange { 20.f };
+	
+	// Soft look at weight. The larger it is, the harder the camera will look at the target.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputParameters, meta = (ClampMin = "0.0", ClampMax = "1.0", EditCondition = "LookAtConstraintType == EComposableCameraLookAtConstraintType::Soft", EditConditionHides))
+	float SoftLookAtWeight { 0.2f };
+
+	// Soft look at interpolator when resuming to the look-at direction.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Instanced, Category = InputParameters, meta = (EditCondition = "LookAtConstraintType == EComposableCameraLookAtConstraintType::Soft", EditConditionHides))
+	UComposableCameraInterpolatorBase* SoftLookAtInterpolator { nullptr };
+
+private:
+	USkeletalMeshComponent* SkeletalMeshComponentForLookAtActor { nullptr };
+	TUniquePtr<TCameraInterpolator<TValueTypeWrapper<FRotator>>> Interpolator_T;
+};
