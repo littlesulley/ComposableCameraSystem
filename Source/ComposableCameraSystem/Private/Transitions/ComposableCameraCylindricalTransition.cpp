02538704e3429b5f6ad9fe96734319e9fde18d88
littlesulley <qinghonghan97@gmail.com>
2025-10-05 13:39:26 +0800

    Cylindrical transition.

---
+++ Source/ComposableCameraSystem/Private/Transitions/ComposableCameraCylindricalTransition.cpp
@@ -0,0 +1,64 @@
+﻿// Copyright Sulley. All rights reserved.
+
+#include "Transitions/ComposableCameraCylindricalTransition.h"
+
+#include "Interpolator/ComposableCameraInterpolatorBase.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UComposableCameraCylindricalTransition::OnBeginPlay_Implementation(float DeltaTime,
+	const FComposableCameraPose& CurrentTargetPose)
+{
+	Interpolator_T = PivotInterpolator ? PivotInterpolator->BuildVector3dInterpolator() : nullptr;
+}
+
+FComposableCameraPose UComposableCameraCylindricalTransition::OnEvaluate_Implementation(float DeltaTime,
+                                                                                        const FComposableCameraPose& CurrentTargetPose)
+{
+	// Position.
+	FComposableCameraRayDefinition StartRay { StartCameraPose.Position, StartCameraPose.Rotation.RotateVector(FVector::ForwardVector), MinimumDistanceFromOrigin };
+	FComposableCameraRayDefinition TargetRay { CurrentTargetPose.Position, CurrentTargetPose.Rotation.RotateVector(FVector::ForwardVector), MinimumDistanceFromOrigin };
+	FComposableCameraNearestPointsOnRaysResult Result = StartRay.FindNearestPointsByOtherRay(TargetRay);
+
+	float BlendPct = (TransitionTime - RemainingTime) / TransitionTime;
+
+	FVector StartPosition = StartCameraPose.Position;
+	FVector StartPivot = Result.FirstPoint;
+	FVector TargetPosition = CurrentTargetPose.Position;
+	FVector TargetPivot = Result.SecondPoint;
+
+	FVector StartDirection = StartPosition - StartPivot;
+	FVector TargetDirection = TargetPosition - TargetPivot;
+	
+	FVector StartProjectedDirection = StartDirection.ProjectOnToNormal(FVector::UpVector);
+	FVector TargetProjectedDirection = TargetDirection.ProjectOnToNormal(FVector::UpVector);
+	float StartRadius = StartProjectedDirection.Length();
+	float TargetRadius = TargetProjectedDirection.Length();
+
+	FVector StartProjectedNormalizedDirection = StartProjectedDirection.GetSafeNormal();
+	FVector TargetProjectedNormalizedDirection = TargetProjectedDirection.GetSafeNormal();
+	
+	FVector ResultVector = FVector::SlerpNormals(StartProjectedNormalizedDirection, TargetProjectedNormalizedDirection, BlendPct);
+	ResultVector *= StartRadius + BlendPct * (TargetRadius - StartRadius);
+	FVector StartResultPosition = StartPosition + (ResultVector - StartProjectedDirection);
+	FVector TargetResultPosition = TargetPosition + (ResultVector - TargetProjectedDirection);
+	FVector ResultPosition = StartResultPosition + BlendPct * (TargetResultPosition - StartResultPosition);
+
+	// Rotation, always looking to the blended pivot.
+	FVector ResultPivot = StartPivot + BlendPct * (TargetPivot - StartPivot);
+	
+	if (Interpolator_T)
+	{
+		Interpolator_T->Reset(StartPivot, TargetPivot);
+		ResultPivot = Interpolator_T->Run(DeltaTime);
+	}
+
+	FRotator ResultRotation = UKismetMathLibrary::FindLookAtRotation(ResultPosition, ResultPivot);
+
+	// Returns output pose.
+	FComposableCameraPose ResultPose {};
+	ResultPose.Position = ResultPosition;
+	ResultPose.Rotation = ResultRotation;
+	ResultPose.FieldOfView = StartCameraPose.FieldOfView + BlendPct * (CurrentTargetPose.FieldOfView - StartCameraPose.FieldOfView);
+	
+	return ResultPose;
+}
---
+++ Source/ComposableCameraSystem/Public/Transitions/ComposableCameraCylindricalTransition.h
@@ -0,0 +1,135 @@
+﻿// Copyright Sulley. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ComposableCameraTransitionBase.h"
+#include "Interpolator/ComposableCameraInterpolatorBase.h"
+#include "ComposableCameraCylindricalTransition.generated.h"
+
+class UComposableCameraInterpolatorBase;
+
+struct FComposableCameraNearestPointsOnRaysResult
+{
+	// The first point.
+	FVector FirstPoint;
+
+	// The second point.
+	FVector SecondPoint;
+
+	// Minimum distance between two rays.
+	float Distance;
+
+	// If two rays are parallel, in which case the points are randomly selected.
+	bool bIsParallel;
+};
+
+struct FComposableCameraRayDefinition
+{
+	FComposableCameraRayDefinition(FVector InOrigin, FVector InDirection)
+		: Origin(InOrigin), Direction(InDirection), MinimumDistance(0.f), bInfiniteDistance(true)
+	{}
+
+	FComposableCameraRayDefinition(FVector InOrigin, FVector InDirection, float InMinimumDistance)
+		: Origin(InOrigin), Direction(InDirection), MinimumDistance(InMinimumDistance), bInfiniteDistance(true)
+	{}
+
+	FComposableCameraNearestPointsOnRaysResult FindNearestPointsByOtherRay(const FComposableCameraRayDefinition& OtherRay)
+	{
+		FComposableCameraNearestPointsOnRaysResult Result;
+		
+		FVector D1 = this->Direction.GetSafeNormal();
+		FVector D2 = OtherRay.Direction.GetSafeNormal();
+		
+		FVector O1 = this->Origin + this->Direction * this->MinimumDistance;
+		FVector O2 = OtherRay.Origin + OtherRay.Direction * OtherRay.MinimumDistance;
+		FVector W0 = O1 - O2;
+		
+		float W1 = FVector::DotProduct(D1, W0);
+		float W2 = FVector::DotProduct(D2, W0);
+		float Dot = FVector::DotProduct(D1, D2);
+		float SquaredDot = FMath::Square(Dot);
+
+		if (FMath::IsNearlyZero(Dot, 1e-2f))
+		{
+			Result.bIsParallel = true;
+
+			if (W2 <= 0.f)
+			{
+				Result.SecondPoint = O2;
+				Result.FirstPoint = O1 + (-W1) * D1;
+				Result.Distance = FVector::Dist(Result.FirstPoint, Result.SecondPoint);
+			}
+			else
+			{
+				Result.FirstPoint = O1;
+				Result.SecondPoint = O2 + W2 * D2;
+				Result.Distance = FVector::Dist(Result.FirstPoint, Result.SecondPoint);
+			}
+
+			return Result;
+		}
+
+		float T1 = (Dot * W2 - W1) / SquaredDot;
+		float T2 = (Dot * W1 - W2) / SquaredDot;
+
+		// Considering if T1 or T2 is negative.
+		if (T1 < 0.f)
+		{
+			T1 = 0.f;
+			T2 = W2 > 0.f ? W2 : 0.0f;
+		}
+		else if (T2 < 0.f)
+		{
+			T2 = 0.f;
+			T1 = -W1 > 0.f ? -W1 : 0.0f;
+		}
+		
+		Result.FirstPoint = O1 + T1 * D1;
+		Result.SecondPoint = O2 + T2 * D2;
+		Result.Distance = FVector::Dist(Result.FirstPoint, Result.SecondPoint);
+		Result.bIsParallel = false;
+		
+		return Result;
+	}
+
+	// Origin of the ray.
+	FVector Origin;
+
+	// Direction of the ray.
+	FVector Direction;
+
+	// Minimum distance from the origin along direction.
+	float MinimumDistance;
+	
+	// Distance of the ray is bInifiniteDistance == false.
+	float Distance;
+
+	// If this ray has an infinite distance.
+	bool bInfiniteDistance;
+};
+
+/**
+ * Cylindrical transition.
+ */
+UCLASS(ClassGroup = ComposableCameraSystem)
+class COMPOSABLECAMERASYSTEM_API UComposableCameraCylindricalTransition : public UComposableCameraTransitionBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnBeginPlay_Implementation(float DeltaTime, const FComposableCameraPose& CurrentTargetPose) override;
+	virtual FComposableCameraPose OnEvaluate_Implementation(float DeltaTime, const FComposableCameraPose& CurrentTargetPose) override;
+
+public:
+	// Maintaining a minimum distance from origin along the camera's looking direction.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinimumDistanceFromOrigin { 10.f };
+
+	// Pivot move interpolator.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
+	UComposableCameraInterpolatorBase* PivotInterpolator;
+
+private:
+	TUniquePtr<TCameraInterpolator<TValueTypeWrapper<FVector3d>>> Interpolator_T;
+};
