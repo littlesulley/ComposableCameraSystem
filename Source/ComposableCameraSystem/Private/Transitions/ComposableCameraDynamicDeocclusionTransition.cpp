3f6cd3406531cc273b7a821a5f412baeca2d7955
littlesulley <494597918@qq.com>
2025-12-31 18:51:56 +0800

    Initial trial for DynamicDeocclusionTransition.

---
+++ Source/ComposableCameraSystem/Private/Transitions/ComposableCameraDynamicDeocclusionTransition.cpp
@@ -0,0 +1,116 @@
+// Copyright Sulley. All rights reserved.
+
+#include "Transitions/ComposableCameraDynamicDeocclusionTransition.h"
+
+#include "ComposableCameraSystemModule.h"
+#include "Core/ComposableCameraPlayerCamaraManager.h"
+#include "EditorHooks/EditorHooks.h"
+#include "Kismet/GameplayStatics.h"
+
+void UComposableCameraDynamicDeocclusionTransition::OnBeginPlay_Implementation(float DeltaTime,
+	const FComposableCameraPose& CurrentTargetPose)
+{
+	if (DrivingTransition)
+	{
+		DrivingTransition->TransitionEnabled(SourceCamera, TargetCamera, StartCameraPose);
+		DrivingTransition->SetTransitionTime(TransitionTime);
+		DrivingTransition->ResetTransitionState();
+	}
+
+	// DrawDebugType.
+	DrawDebugType =
+		TargetCamera->GetOwningPlayerCameraManager()->bDrawDebugInformation ?
+		EDrawDebugTrace::ForOneFrame :
+		EDrawDebugTrace::None;
+
+#if WITH_EDITOR
+	if (!FIsSimulatingInEditor::GetIsSimulatingInEditor())
+	{
+		DrawDebugType = EDrawDebugTrace::None;
+	}
+#endif
+
+	// Ignored actors.
+	for (TSoftClassPtr<AActor> ActorType: ActorTypesToIgnore)
+	{
+		if (ActorType.IsValid())
+		{
+			TArray<AActor*> IgnoredActors;
+			UGameplayStatics::GetAllActorsOfClass(this, ActorType.Get(), IgnoredActors);
+			ActorsToIgnore.Append(IgnoredActors);
+		}
+	}
+}
+
+FComposableCameraPose UComposableCameraDynamicDeocclusionTransition::OnEvaluate_Implementation(float DeltaTime,
+	const FComposableCameraPose& CurrentTargetPose)
+{
+	if (!DrivingTransition)
+	{
+		UE_LOG(LogComposableCameraSystem, Warning, TEXT("DrivingTransition is not valid in ComposableCameraDynamicDeocclusionTransition."));
+		return FComposableCameraPose{};
+	}
+	
+	FComposableCameraPose BasePose = DrivingTransition->Evaluate(DeltaTime, CurrentTargetPose);
+	FComposableCameraPose CandidatePose = BasePose;
+	CandidatePose.Position = PreviousOffset + BasePose.Position;
+	FVector AggregateOffset = FVector::ZeroVector;
+
+	// For each feeler.
+	for (const auto& Feeler : Feelers)
+	{
+		FVector StartPosition = Feeler.GetRayStartPosition(CandidatePose);
+		FVector EndPosition = Feeler.GetRayEndPosition(CandidatePose);
+
+		FHitResult Hit;
+		UKismetSystemLibrary::SphereTraceSingle(
+			this,
+			StartPosition,
+			EndPosition,
+			Feeler.Radius,
+			TraceChannel,
+			true,
+			ActorsToIgnore,
+			DrawDebugType,
+			Hit,
+			true);
+
+		if (Hit.bBlockingHit && Feeler.StrengthCurve)
+		{
+			FVector Offset = CandidatePose.Rotation.RotateVector(Feeler.Offset.GetSafeNormal());
+			AggregateOffset += Offset * Feeler.StrengthCurve->GetFloatValue(Hit.Distance);
+		}
+	}
+
+	if (AggregateOffset.IsZero())
+	{
+		ElapsedWaitingTime += DeltaTime;
+	}
+	else
+	{
+		ElapsedWaitingTime = 0.f;
+	}
+
+	if (ElapsedWaitingTime >= ResumeWaitingTime || (GetTransitionTime() - GetRemainingTime()) / GetTransitionTime() >= DeadPercentage)
+	{
+		AggregateOffset = FMath::VInterpTo(
+			PreviousOffset,
+			FVector::ZeroVector,
+			DeltaTime,
+			DeocclusionSpeed
+		);
+	}
+	else
+	{
+		AggregateOffset = FMath::VInterpTo(
+			PreviousOffset,
+			PreviousOffset + AggregateOffset,
+			DeltaTime,
+			DeocclusionSpeed
+		);
+	}
+	
+	CandidatePose.Position = BasePose.Position + AggregateOffset;
+	PreviousOffset = AggregateOffset;
+	return CandidatePose;
+}
---
+++ Source/ComposableCameraSystem/Public/Transitions/ComposableCameraDynamicDeocclusionTransition.h
@@ -0,0 +1,102 @@
+// Copyright Sulley. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ComposableCameraTransitionBase.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "ComposableCameraDynamicDeocclusionTransition.generated.h"
+
+class UCurveFloat;
+
+USTRUCT(BlueprintType)
+struct FComposableCameraRayFeeler
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "-180", ClampMax = "180"))
+	float Yaw;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "-90", ClampMax = "90"))
+	float Pitch;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Length;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Radius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Offset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCurveFloat* StrengthCurve;
+
+public:
+	FVector GetRayStartPosition(const FComposableCameraPose& Pose) const
+	{
+		return Pose.Position;
+	}
+
+	FVector GetRayEndPosition(const FComposableCameraPose& Pose) const
+	{
+		FRotator R { Pitch, Yaw, 0.f };
+		FVector Ray = R.RotateVector(FVector::ForwardVector) * Length;
+		Ray = Pose.Rotation.RotateVector(Ray);
+		return GetRayStartPosition(Pose) + Ray;
+	}
+};
+
+/**
+ * A dynamically deocclusive transition.
+ */
+UCLASS(ClassGroup = ComposableCameraSystem)
+class COMPOSABLECAMERASYSTEM_API UComposableCameraDynamicDeocclusionTransition : public UComposableCameraTransitionBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnBeginPlay_Implementation(float DeltaTime, const FComposableCameraPose& CurrentTargetPose) override;
+	virtual FComposableCameraPose OnEvaluate_Implementation(float DeltaTime, const FComposableCameraPose& CurrentTargetPose) override;
+
+public:
+	// The driving transition.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced)
+	UComposableCameraTransitionBase* DrivingTransition;
+	
+	// Feelers for dynamic occlusion detection.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FComposableCameraRayFeeler> Feelers;
+
+	// Collision channel for feeler trace.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TEnumAsByte<ETraceTypeQuery> TraceChannel;
+	
+	// Actor types to ignore for feelers.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<TSoftClassPtr<AActor>> ActorTypesToIgnore;
+
+	// Deocclusion speed.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float DeocclusionSpeed { 1.f };
+
+	// Should wait for such time to resume if no occlusion is detected.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float ResumeWaitingTime { 0.2f };
+
+	// When the transition process exceeds this percentage, will resume to the base pose ignoring any occlusion.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0", ClampMax = "1"))
+	float DeadPercentage { 0.8f };
+
+	// When the transition process exceeds this percentage, will resume to the base pose ignoring any occlusion.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0", ClampMax = "1"))
+	float ResumeSpeed { 0.8f };
+
+private:
+	FVector PreviousOffset { FVector::ZeroVector };
+	float ElapsedWaitingTime { 0.f };
+	
+	TArray<AActor*> ActorsToIgnore;
+	EDrawDebugTrace::Type DrawDebugType;
+};
